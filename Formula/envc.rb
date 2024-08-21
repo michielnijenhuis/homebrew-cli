ca280b4183b73cceb6cf3397cf32dfd2ac392f6c
Michiel Nijenhuis <michiel@totaldesign.com>
2024-08-21 14:55:31 +0200

    fix: sha256

--- Formula/envc.rb
+++ Formula/envc.rb
@@ -2,7 +2,7 @@ class Envc < Formula
     desc "A program that prints all variables in the available .env file(s), sorted alphabetically"
     homepage "https://github.com/michielnijenhuis/env-check"
     url "https://github.com/michielnijenhuis/envc/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "33a8357ec0cf029c691567af905f53b958e4b7222b54f6d882dc8c583d194b6f"
+    sha256 "da0d7619a01230aa2821ac4261947cb858a40313bb295ca5ba0ee9632cc0a3d4"
     license "MIT"
     
     depends_on "make"
