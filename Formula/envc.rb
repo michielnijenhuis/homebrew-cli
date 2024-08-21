77ebec3831093623ab2dd5a0615f581fba8760bf
Michiel Nijenhuis <michiel.nijenhuis@gmail.com>
2024-08-21 19:33:42 +0000

    envc 2.2.0

--- Formula/envc.rb
+++ Formula/envc.rb
@@ -1,8 +1,8 @@
 class Envc < Formula
     desc "A program that prints all variables in the available .env file(s), sorted alphabetically"
     homepage "https://github.com/michielnijenhuis/env-check"
-    url "https://github.com/michielnijenhuis/envc/archive/refs/tags/v2.1.0.tar.gz"
-    sha256 "705a0e405b0113d72cb99a9438974fe1beafe4e082eb66cfa275cdd21f51d8eb"
+    url "https://github.com/michielnijenhuis/envc/archive/refs/tags/v2.2.0.tar.gz"
+    sha256 "3a8e7ef6beb6d64d9351f70566477d8bd631008a07c498fe4011633052a3bcb5"
     license "MIT"
     
     depends_on "go"
